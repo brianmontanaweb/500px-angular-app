5df3644a86572952ce034558f082b03bb8c5889b
Brian Montana <Brian.Montana@blastradius.com>
2019-01-02 12:32:10 -0600

    Migrating from 5 to 7 and updating missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
 import { PhotoComponent } from './photos/photo/photo.component';
 import { Api500pxService } from './shared';
 import { SearchComponent } from './search/search.component';
 import { StickyMenuComponent } from './sticky-menu/sticky-menu.component';
+import { RadioComponent } from './radio/radio.component';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { StickyMenuComponent } from './sticky-menu/sticky-menu.component';
     PhotosComponent,
     PhotoComponent,
     SearchComponent,
-    StickyMenuComponent
+    StickyMenuComponent,
+    RadioComponent,
   ],
   imports: [
     BrowserModule
