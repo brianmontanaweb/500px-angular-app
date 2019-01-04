c485e20f45069d7b5fab4ed550ff4c984bdea0a7
Brian Montana <Brian.Montana@blastradius.com>
2019-01-04 15:20:38 -0600

    Removing component styling

--- src/app/sticky-menu/sticky-menu.component.ts
+++ src/app/sticky-menu/sticky-menu.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sticky-menu.component.scss']
 })
 export class StickyMenuComponent implements OnInit {
+  activeMenu = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  stickyToggle = (active: boolean = this.activeMenu) => {
+    this.activeMenu = !active;
   }
 
 }
